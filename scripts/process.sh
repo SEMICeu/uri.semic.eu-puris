4bbae2409ad6d612a41c15b4adad2b16cab86124
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2021-12-22 09:46:42 +0000

    add updated RDF descriptions from CCCEV 2.00 release

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use the following sources to process the m8g domain
-wget https://github.com/bertvannuffelen/semiceu-generated/raw/main/doc/vocabularium/cccev-voc/voc/cccev_en.ttl
+wget https://github.com/SEMICeu/CCCEV/raw/gh-pages/releases/2.00-draft/voc/cccev_en.ttl
 wget https://github.com/catalogue-of-services-isa/CPSV-AP/raw/master/releases/2.2/SC2015DI07446_D02.02_CPSV-AP_v2.2_RDF_Schema_v1.00.ttl
 
 ./publish.sh m8g cccev_en.ttl
